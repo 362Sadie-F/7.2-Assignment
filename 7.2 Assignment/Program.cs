fe3d9fee23a02c0eed479553a0e1840dc28ee24f
362Sadie-F <princessmae.fleming@gmail.com>
2026-03-26 12:51:36 -0400

    task 1

--- 7.2 Assignment/Program.cs
+++ 7.2 Assignment/Program.cs
@@ -4,6 +4,36 @@
     {
         static void Main(string[] args)
         {
+            
+
+            Console.Write("Enter your message and I'll type it 5 times: ");
+            string message = Console.ReadLine();
+
+            for (int i = 1; i <=5; i = i + 1)
+            {
+                Console.WriteLine(i + ". " + message);
+            }
+            Console.ReadLine();
+            Console.Clear();
+
+            //task 1 Counting down "Blast Off!"
+            Console.WriteLine("Counting Down!");
+
+            for (int i = 10; i >= 0; i = i - 1)
+            {
+                Console.WriteLine(i + "... ");
+            }
+            Console.WriteLine("Blast Off!");
+            Console.ReadLine();
+            Console.Clear();
+
+            //task 2 X & Y
+
+
+            //task 3 For Loop Fun
+
+
+
             Console.WriteLine();
         }
     }
